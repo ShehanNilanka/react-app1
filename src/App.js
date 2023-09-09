6a29e2c18f1d2c90becbdd79450024a586a379e2
shehan <nilankashehan@gmail.com>
2023-09-09 19:27:37 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+          Helloo my react app!!!
+          <button onClick={() => h()}>Click This Button </button>
+      </header>
+    </div>
+  );
+}
+
+
+function h() {
+  console.log("Button Clicked");
+}
+export default App;
